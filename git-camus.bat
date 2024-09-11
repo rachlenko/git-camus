5e52cdfc1a9c24826358cf56b125e449caf62c50
Evgeny Rachlenko <evgeny.rachlenko@gmail.com>
2024-09-11 21:57:57 +0100

    init web site

---
+++ git-camus.bat
@@ -0,0 +1,3 @@
+set GITHUB_COPILOT_TOKEN="YOUR_TOKEN" 
+set PYTHONIOENCODING="utf-8"
+python3 git-camus.py %*
---
+++ git-camus.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export GITHUB_COPILOT_TOKEN="YOUR_TOKEN" 
+python3 git-camus.py $_ 
