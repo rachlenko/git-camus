faa222140ba03bef32f638dcffd004504b19c82c
Evgeny Rachlenko <evgeny.rachlenko@gmail.com>
2025-01-11 14:09:25 +0000

    added poe plugin allows to run shell commands from the poetry directly without extra make files etc ..

--- tests/test_git_camus.py
+++ tests/test_git_camus.py
@@ -1,13 +1,9 @@
 import pytest
 import sys
+
 # import subprocess
 # import sys
 
+
 def test_check_version():
-    assert sys.version_info <= (3, 12, 6, 'final', 0)
-
-
-
-
-
-    
+    assert sys.version_info <= (3, 12, 6, "final", 0)
